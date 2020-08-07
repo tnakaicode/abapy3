990903eb73840aba1cc0ff77ea278fe6e67722b0
tnakaicode <tnakaicode@gmail.com>
2020-08-08 01:12:31 +0900

    fgormat

--- doc/example_code/postproc/HistoryOutput-average.py
+++ doc/example_code/postproc/HistoryOutput-average.py
@@ -2,13 +2,13 @@ from abapy.postproc import HistoryOutput
 from math import sin, pi
 N = 100
 hist = HistoryOutput()
-time = [pi / 2 * float(i)/N for i in range(N+1)]
+time = [pi / 2 * float(i) / N for i in range(N + 1)]
 data = [sin(t) for t in time]
-hist.add_step(time_step = time, data_step = data)
+hist.add_step(time_step=time, data_step=data)
 time2 = [10., 11.]
 data2 = [1., 1.]
-hist.add_step(time_step = time2, data_step = data2)
+hist.add_step(time_step=time2, data_step=data2)
 sol = 2. / pi + 1.
-print ('Print computed value:', hist.average())
-print ('Analytic solution:', sol)
-print ('Relative error: {0:.4}%'.format( (hist.average() - sol)/sol * 100.))
+print('Print computed value:', hist.average())
+print('Analytic solution:', sol)
+print('Relative error: {0:.4}%'.format((hist.average() - sol) / sol * 100.))
